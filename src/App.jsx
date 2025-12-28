dde2464f449454c44a7dd1e654d0ea7be11bafac
Rayhan Chowdhury <alvi.rayhan@gmail.com>
2025-12-28 14:58:18 +0400

    initital commit

---
+++ src/App.jsx
@@ -0,0 +1,110 @@
+import React, { use } from "react";
+import Search from "./components/Search";
+import { useState, useEffect } from "react";
+import Spinner from "./components/Spinner";
+
+const API_BASE_URL = "https://api.themoviedb.org/3";
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+
+const API_OPTIONS = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization: `Bearer ${API_KEY}`,
+  },
+};
+
+const App = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const [movieList, setMovieList] = useState([]);
+
+  const [isLoading, setIsLoading] = useState(true);
+
+  const fetchMovies = async (query) => {
+    setIsLoading(true);
+    setErrorMessage("");
+
+    try {
+      const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+      const response = await fetch(endpoint, API_OPTIONS);
+
+      if (!response.ok) {
+        throw new Error("Failed to fetch movies");
+      }
+      const data = await response.json();
+
+      if (data.response === "False") {
+        setErrorMessage(data.error || "No movies found.");
+        setMovieList([]);
+        return;
+      }
+
+      setMovieList(data.results);
+    } catch (error) {
+      console.error("Error fetching movies:", error);
+      setErrorMessage("Failed to fetch movies. Please try again later.");
+    } finally {
+      setIsLoading(false);
+    }
+  }; 
+
+  useEffect(() => {
+    fetchMovies();
+  }, []);
+
+  return (
+    <main
+      style={{
+        backgroundImage: "url('/BG.png')",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+      }}
+    >
+      <div className="pattern">
+        <div className="wrapper">
+          <header>
+            <img src="./hero-img.png" alt="Hero Banner" />
+            <h1>
+              Find <span className="text-gradient">Movies</span> You'll Enjoy
+              Without the Hassle
+            </h1>
+            <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+          </header>
+          <section className="all-movies">
+            <h2>All Movies</h2>
+            {isLoading ? (
+              <Spinner />
+            ) : errorMessage ? (
+              <p className="text-red-500">{errorMessage}</p>
+            ) : (
+              <ul>
+                {movieList.map((movie) => (
+                  <li key={movie.id} className="movie-card">
+                    <img
+                      src={
+                        movie.poster_path
+                          ? `https://image.tmdb.org/t/p/w500${movie.poster_path}`
+                          : "/no-image-available.png"
+                      }
+                      alt={movie.title}
+                    />
+                    <div className="movie-info">
+                      <h3>{movie.title}</h3>
+                      <p>Rating: {movie.vote_average}</p>
+                    </div>
+                  </li>
+                ))}
+              </ul>
+            )}
+          </section>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default App;
---
+++ src/components/Spinner.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { RiseLoader } from 'react-spinners'
+
+const Spinner = () => {
+  return (
+    <div className="flex justify-center mt-10">
+      <RiseLoader color="#D6C7FF" />
+    </div>
+  )
+}
+
+export default Spinner
