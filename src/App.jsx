cefd4826bf7c606a6ab143416d1b11de9ee73651
Rayhan Chowdhury <alvi.rayhan@gmail.com>
2025-12-28 15:43:43 +0400

    Fix fetchMovies function to handle default query and update useEffect dependency

--- src/App.jsx
+++ src/App.jsx
@@ -23,12 +23,12 @@ const App = () => {
 
   const [isLoading, setIsLoading] = useState(true);
 
-  const fetchMovies = async (query) => {
+  const fetchMovies = async (query ='') => {
     setIsLoading(true);
     setErrorMessage("");
 
     try {
-      const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+      const endpoint = query ? `${API_BASE_URL}/search/movie?query=${encodeURIComponent(query)}` : `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
       const response = await fetch(endpoint, API_OPTIONS);
 
       if (!response.ok) {
@@ -52,8 +52,8 @@ const App = () => {
   }; 
 
   useEffect(() => {
-    fetchMovies();
-  }, []);
+    fetchMovies(searchTerm);
+  }, [searchTerm]);
 
   return (
     <main
